e07b9e10fe2f607978b06965916c5b3c021406c9
電車君 <densyakun1128@gmail.com>
2017-05-08 14:48:15 +0900

    間違えてファイルをもとに戻してしまったと思い作り直したがファイルが残っていたのでマージ気味にコミット

---
+++ Assets/Blueprint/Common/Scripts/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#elif UNITY_WEBPLAYER
+			//Application.OpenURL (webplayerQuitURL);
+			#else
+			Application.Quit ();
+			#endif
+		}
+	}
+}
