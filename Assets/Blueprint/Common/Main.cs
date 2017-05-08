bb879ef8566d16e0aa9fbf1c3265654770889597
電車君 <densyakun1128@gmail.com>
2017-05-08 14:01:02 +0900

    test-900 スプラッシュ画面とUIのテスト

---
+++ Assets/Blueprint/Common/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			print ("");
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+			Application.Quit ();
+			#endif
+		}
+	}
+}
---
+++ Assets/Blueprint/Densyakun/900/TestButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour {
+	public Text text;
+	public int a = 0;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void Test(int n) {
+		a += n;
+		text.text = "" + a;
+	}
+}
