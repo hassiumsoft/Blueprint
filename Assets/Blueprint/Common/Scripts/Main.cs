b9afede114768c419020039a33f1dc5de8fce399
電車君 <densyakun1128@gmail.com>
2017-05-08 16:01:12 +0900

    タイトル画面（仮）を作成

--- Assets/Blueprint/Common/Scripts/Main.cs
+++ Assets/Blueprint/Common/Scripts/Main.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
-	
+	//public Camera c;
+	public Canvas title;
+
 	void Start () {
-		
+		showTitle (true);
 	}
 
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			#if UNITY_EDITOR
-			UnityEditor.EditorApplication.isPlaying = false;
-			#elif UNITY_WEBPLAYER
-			//Application.OpenURL (webplayerQuitURL);
-			#else
-			Application.Quit ();
-			#endif
+			quit ();
 		}
+	}
+
+	public void quit () {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#elif !UNITY_WEBPLAYER
+		Application.Quit ();
+		#endif
+	}
+
+	public void showTitle (bool show) {
+		title.enabled = show;
 	}
 }
