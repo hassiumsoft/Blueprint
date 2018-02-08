19b5606123a7674dc19a22b37ee4da02601c6aef
Zerosaka Makoto <kanjyosen0@gmail.com>
2018-02-08 21:40:01 +0900

    途中ですが取り敢えずブランチついでにコミット＆プッシュ。 勿論別のバージョンである「奇想天外β」としてコミット＆プッシュ（なんかめんどくさいのでC&Pで）していきます。 ＋デバッグモードへの門だけを追加（その先はない） ＋マップモードを入力して変更する用の入力欄だけを追加（その先はない）

--- Assets/Blueprint/UI/SettingPanel.cs
+++ Assets/Blueprint/UI/SettingPanel.cs
@@ -4,12 +4,31 @@ using UnityEngine.UI;
 public class SettingPanel : BPPanel {
 	public Text drawDistanceText;
 	public Slider drawDistanceSlider;
+    public InputField secretgate;
+    public string pass;
+
+    void Start()
+    {
+        secretgate = GetComponent<InputField>();
+
+        ResetString(secretgate.text);
+        ActivateIF(secretgate);
+
+        string pass = 66666666;
+    }
 
 	void Update () {
 		drawDistanceText.text = "描画距離: " + (int)drawDistanceSlider.value;
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			show (false);
 		}
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (secretgate.text == pass)
+            {
+
+            }
+        }
 	}
 
 	void load () {
@@ -17,6 +36,16 @@ public class SettingPanel : BPPanel {
 		drawDistanceSlider.maxValue = Main.MAX_DRAW_DISTANCE;
 		drawDistanceSlider.value = Main.drawDistance;
 	}
+
+    void ResetString(string A)
+    {
+        A = "";
+    }
+
+    void ActivateIF(InputField A)
+    {
+        A.ActivateInputField();
+    }
 
 	public void show (bool show) {
 		if (show) {
